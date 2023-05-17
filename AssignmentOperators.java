8de24a0d242ab6ab8bc756e11415bd7cef43008f
Sumit Jethani <sjethani651@gmail.com>
2023-05-17 19:20:31 +0530

    commit with assignement 13 may completed and pushed

---
+++ AssignmentOperators.java
@@ -0,0 +1,25 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int num = 10;
+
+        // After addition
+        num += 2; // Equivalent to num = num + 2;
+        System.out.println("After addition: num = " + num);
+
+        // After subtraction
+        num -= 4; // Equivalent to num = num - 4;
+        System.out.println("After subtraction: num = " + num);
+
+        // After multiplication
+        num *= 7; // Equivalent to num = num * 7;
+        System.out.println("After multiplication: num = " + num);
+
+        // After division
+        num /= 4; // Equivalent to num = num / 4;
+        System.out.println("After division: num = " + num);
+
+        // After modulus
+        num %= 12; // Equivalent to num = num % 12;
+        System.out.println("After modulus: num = " + num);
+    }
+}
