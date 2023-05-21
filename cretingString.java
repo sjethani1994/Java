f0498fb1216eef6bf3dc2caee1c4c6ed726aa769
Sumit Jethani <sjethani651@gmail.com>
2023-05-21 20:05:40 +0530

    committing with new files added in main

--- cretingString.java
+++ cretingString.java
@@ -3,6 +3,11 @@ public class cretingString {
         // String s1 = "hello";
         // String s2 = "Hello";
 
+        // if (s1 == s2) {
+        // System.out.println("Same.");
+        // } else {
+        // System.out.println("different");
+        // }
         String s1 = new String("baaa");
         String s2 = new String("aaab");
 
@@ -11,10 +16,5 @@ public class cretingString {
         System.out.println(s1.equals(s2));
         System.out.println(s1.equalsIgnoreCase(s2));
         System.out.println(s2.compareTo(s1));
-        // if (s1 == s2) {
-        //     System.out.println("Same.");
-        // } else {
-        //     System.out.println("different");
-        // }
     }
 }
