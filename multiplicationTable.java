1d3bdc4758a162f7a3c01ad4120dbad9f4bcacf7
Sumit Jethani <sjethani651@gmail.com>
2023-06-25 19:56:08 +0530

    commit with assignement added

--- multiplicationTable.java
+++
@@ -1,9 +0,0 @@
-public class multiplicationTable {
-    public static void main(String[] args) {
-        int number = 5;
-
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(number + " x " + i + " = " + number * i);
-        }
-    }
-}
